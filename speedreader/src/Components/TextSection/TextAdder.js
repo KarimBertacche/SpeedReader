dba9cfe438a534e9c35c86026869ad3d3bc8c7d9
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-13 14:56:39 +0200

    Feature: added styling for input and icon in AddText component

--- speedreader/src/Components/TextSection/TextAdder.js
+++ speedreader/src/Components/TextSection/TextAdder.js
@@ -5,7 +5,34 @@ import styled from 'styled-components';
 
 const StyledTextAdder = styled.div`
 display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+color: white;
 
+.input-plus {
+display: flex;
+justify-content: space-around;
+align-items: center;
+width: 30%;
+}
+
+i {
+  font-size: 4rem;
+  transition: .6s ease-in-out;
+
+  &:hover {
+    color: black;
+  }
+}
+
+input {
+font-size: 1.5rem;
+text-align: center;
+width: 70%;
+height: 25rem;
+border-radius: .5rem;
+}
 `;
 
 class TextAdder extends React.Component {
@@ -34,10 +61,12 @@ this.setState({
     return ( 
       <StyledTextAdder>
         <h1>Add text to train your reading skills !</h1>
+        <idv className="input-plus">
       <input type="text" name="text" value={this.state.text} onChange={this.changeHandler} placeholder="Text" />
       <i
       onClick={this.addTextToRedux} 
       className="fa fa-plus-square"/>
+      </idv>
       </StyledTextAdder>
      );
   }
